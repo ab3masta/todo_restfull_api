7383adfaeb2a45a5d574019476792b10779bb155
Abdou Abarchi Aboubacar <57944650+AbdouAbarchiAboubacar@users.noreply.github.com>
2023-04-09 00:42:00 +0100

    Add Task CRUD operation to MongoDB.

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -3,8 +3,16 @@ from typing import Optional
 
 
 class Task(BaseModel):
-    id: int
     title: str
     description: Optional[str] = None
     completed: bool
     due_date: Optional[str] = None
+
+    def dict(self) -> dict:
+        return {
+            "title": str(self.title),
+            "description": self.description,
+            "completed": self.completed,
+            "due_date":  self.due_date,
+        }
+
