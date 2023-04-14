a1110ac21d887b643782fcde5a5a1767351d2cdf
Abdou Abarchi Aboubacar <57944650+AbdouAbarchiAboubacar@users.noreply.github.com>
2023-04-14 20:09:53 +0100

    Add tests on firebase and postgresql crud methods

--- app/routers/mongo_routes.py
+++ app/routers/mongo_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.utils.mongodb_crud import *
 from app.schemas.schemas import Task
+from app.schemas.schemas import Response
 
 router = APIRouter()
 
@@ -8,30 +9,51 @@ router = APIRouter()
 @router.post("/tasks")
 async def mongodb_create_task(task: Task):
     response = mongo_create_task(task=task)
-    return {"message": "/task POST. {}".format(response)}
+    if response is not None:
+        return Response(status="Ok",
+                        code="200",
+                        message="Task Created", result=None).dict(exclude_none=True)
+    else:
+        raise HTTPException(status_code=401, detail="Task not created")
 
 
 @router.get("/tasks")
 async def mongodb_get_tasks(skip: int = 0, limit: int = 100):
     tasks = mongo_get_tasks(skip=skip, limit=limit)
-    return {"message": "/tasks GET", "tasks": tasks}
+    return Response(status="Ok",
+                    code="200",
+                    message="Successfully get all tasks", result=tasks).dict(exclude_none=True)
 
 
 @router.get("/tasks/{taskId}")
 async def mongodb_get_task(taskId: str):
     task = mongo_get_task(taskId=taskId)
-    if task is None:
-        return HTTPException(status_code=404, detail="Todo not found")
-    return {"message": "/tasks/{} GET".format(taskId), "tasks": task}
+    if task is not None:
+        return Response(status="Ok",
+                        code="200",
+                        message="Successfully get task", result=task).dict(exclude_none=False)
+    else:
+        return Response(status="Ok",
+                        code="200",
+                        message="Task with this given id not found").dict(exclude_none=False)
 
 
 @router.put("/tasks/{taskId}")
 async def mongodb_update_task(taskId: str, task: Task):
     response = mongo_update_task(taskId=taskId, task=task)
-    return {"message": "/tasks/{} PUT. {}".format(taskId, response)}
+    if response is not None:
+        return Response(status="Ok",
+                        code="200",
+                        message="Successfully update task").dict(exclude_none=False)
+    else:
+        return Response(status="Ok",
+                        code="200",
+                        message="Task with this given id not found").dict(exclude_none=False)
 
 
 @router.delete("/usemongodb/tasks/{taskId}")
 async def mongodb_delete_task(taskId: str):
     response = mongo_delete_task(taskId=taskId)
-    return {"message": "/tasks/{} DELETE. {}".format(taskId, response)}
+    return Response(status="Ok",
+                    code="200",
+                    message="Successfully delete task", result=response).dict(exclude_none=False)
--- tests/test_mongodb.py
+++ tests/test_mongodb.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_create_task():
     task = Task(title="New Task create by test_firebase.py",
                 description="Decription of new Task create by test_firebase.py", completed=False)
-    response = client.post("/use-firebase/tasks/", json=task.dict())
+    response = client.post("/use-mongodb/tasks/", json=task.dict())
     successResponse = {
         "message": "Task Created"
     }
@@ -17,7 +17,7 @@ def test_create_task():
 
 
 def test_get_tasks():
-    response = client.get("/use-firebase/tasks/")
+    response = client.get("/use-mongodb/tasks/")
     successResponse = {
         "message": "Successfully get all tasks"
     }
@@ -27,8 +27,12 @@ def test_get_tasks():
 
 def test_get_task():
     tastId = "-NSwFSzM1GNHdWzMB9mh"
-    response = client.get("/use-firebase/tasks/{}".format(tastId))
+    response = client.get("/use-mongodb/tasks/{}".format(tastId))
+    successResponse = {
+        "message": "Successfully get task"
+    }
     assert response.status_code == 200
+    assert response.json()["message"] == successResponse["message"]
 
 
 def test_update_task():
@@ -36,14 +40,18 @@ def test_update_task():
     task = Task(title="Update New Task create by test_firebase.py",
                 description="Update Decription of new Task create by test_firebase.py", completed=False)
     response = client.put(
-        "/use-firebase/tasks/{}".format(tastId), json=task.dict())
+        "/use-mongodb/tasks/{}".format(tastId), json=task.dict())
+    successResponse = {
+        "message": "Successfully update task"
+    }
     assert response.status_code == 200
+    assert response.json()["message"] == successResponse["message"]
 
 
 def test_delete_task():
     tastId = "-NSwFSzM1GNHdWzMB9mh"
     response = client.delete(
-        "/use-firebase/tasks/{}".format(tastId))
+        "/use-mongodb/tasks/{}".format(tastId))
     successResponse = {
         "message": "Successfully delete task"
     }
